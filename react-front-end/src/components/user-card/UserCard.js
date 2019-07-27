29e860c3a7298f62b66be99878c79cc51cd49b2b
damdauvaotran <ngokhoaisan1999@gmail.com>
2019-07-27 22:44:43 +0700

    add swipe

--- react-front-end/src/components/user-card/UserCard.js
+++ react-front-end/src/components/user-card/UserCard.js
@@ -1,8 +1,19 @@
-import React, {Component} from 'react'
-import  {Card}from 'antd'
+import React, { Component } from 'react';
+import { Card } from 'antd';
+const { Meta } = Card;
 
-class UserCard extends Component{
-    render(){
+class UserCard extends Component {
+    render() {
 
+        return (
+            <div>
+                <Card style={{ width: 375 }}
+                    cover={<img draggable="false" style={{ maxWidth: 375, maxHeight: 567, objectFit: 'cover' }} alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}>
+                    <Meta style={{userSelect: 'none'}} title="Nguyên" description="Học sinh" />
+                </Card>
+            </div>
+        )
     }
 }
+
+export default UserCard
