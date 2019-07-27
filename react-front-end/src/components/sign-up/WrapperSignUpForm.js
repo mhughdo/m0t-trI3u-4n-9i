7c0fa32bdedd8a023df79666e6fa20de1d2aab47
Manh Hung <hungdm1211@gmail.com>
2019-07-27 21:27:12 +0700

    wip

--- react-front-end/src/components/sign-up/WrapperSignUpForm.js
+++ react-front-end/src/components/sign-up/WrapperSignUpForm.js
@@ -1,207 +1,209 @@
-import React, { Component } from 'react'
-import { Form, Icon, Input, Button, Checkbox, Row, Col, Select, message } from 'antd';
+import React, {Component} from 'react'
+import {Form, Icon, Input, Button, Checkbox, Row, Col, Select, message} from 'antd'
+import {auth, createUserProfileDocument} from '../../firebase/firebaseUtils'
 
-const { Option } = Select;
+const {Option} = Select
 const _fieldDecorator = {
+    name: {
+        rules: [{required: true, message: 'Please input your name!'}],
+    },
     username: {
-        rules: [{ required: true, message: 'Please input your username!' }],
+        rules: [{required: true, message: 'Please input your username!'}],
     },
     password: {
-        rules: [{ required: true, message: 'Please input your Password!' }],
+        rules: [{required: true, message: 'Please input your Password!'}],
     },
     age: {
         initialValue: '<20',
-        rules: [{ required: true, message: 'Please input your age!' }],
+        rules: [{required: true, message: 'Please input your age!'}],
     },
     height: {
         rules: [
-
-            { required: true, message: 'Please input your Height!' },
+            {required: true, message: 'Please input your Height!'},
             // { type: 'number', message: 'Chiều cao phải là số!' }
         ],
     },
     location: {
         initialValue: 1,
-        rules: [{ required: true, message: 'Please choose your location!' }],
+        rules: [{required: true, message: 'Please choose your location!'}],
     },
     gender: {
         initialValue: 'male',
-        rules: [{ required: true, message: 'Please choose your gender!' }],
+        rules: [{required: true, message: 'Please choose your gender!'}],
     },
     job: {
-        rules: [{ required: true, message: 'Please choose your job!' }],
+        rules: [{required: true, message: 'Please choose your job!'}],
     },
     sport: {
-        rules: [{ required: true, message: 'Please choose your favourite sport!' }],
-    }
+        rules: [{required: true, message: 'Please choose your favourite sport!'}],
+    },
 }
 
 const formItemLayout = {
     labelCol: {
-        xs: { span: 24 },
-        sm: { span: 8 },
+        xs: {span: 24},
+        sm: {span: 8},
     },
     wrapperCol: {
-        xs: { span: 24 },
-        sm: { span: 16 },
+        xs: {span: 24},
+        sm: {span: 16},
     },
-};
-
-
-
+}
 
 class SignUpForm extends Component {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
             signUpLoading: false,
-            location: {
-            }
+            location: {},
         }
     }
 
-    handleSubmit = (e) => {
-        e.preventDefault();
+    handleSubmit = e => {
+        const {lo, la} = this.state.location
+        e.preventDefault()
         this.props.form.validateFieldsAndScroll(async (err, values) => {
             if (!err) {
                 this.setState({
-                    signUpLoading: true
+                    signUpLoading: true,
                 })
-                console.log('Received values of form: ', values);
-                const { la, lo } = this.state.location;
-                console.log(la, lo);
+                console.log('Received values of form: ', values)
+                const {username, password} = values
+                const {user} = await auth.createUserWithEmailAndPassword(username, password)
+                console.log(user)
+                // console.log(la, lo)
                 if (la && lo) {
                     // Todo call API
+
+                    const allValues = {...values, lo, la}
                 } else {
                     message.error('Ban chưa cấp phép cho thông tin vị trí, Hãy thử lại')
                 }
             }
-        });
+        })
     }
 
     handleGetLocation = () => {
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((position) => {
+            navigator.geolocation.getCurrentPosition(position => {
                 this.setState({
                     location: {
                         la: position.coords.latitude,
-                        lo: position.coords.longitude
-                    }
+                        lo: position.coords.longitude,
+                    },
                 })
-            });
+            })
         } else {
-            console.log("Geolocation is not supported by this browser.");
+            console.log('Geolocation is not supported by this browser.')
         }
     }
+
     render() {
-        const { getFieldDecorator } = this.props.form;
+        const {getFieldDecorator} = this.props.form
         return (
-            <Form {...formItemLayout} onSubmit={this.handleSubmit} style={{ minWidth: '500px' }}>
-                <Form.Item label="Tên đăng nhập">
+            <Form {...formItemLayout} onSubmit={this.handleSubmit} style={{minWidth: '500px'}}>
+                <Form.Item label='Tên'>
+                    {getFieldDecorator('name', _fieldDecorator.name)(
+                        <Input prefix={<Icon type='user' style={{color: 'rgba(0,0,0,.25)'}} />} placeholder='Name' />
+                    )}
+                </Form.Item>
+                <Form.Item label='Tên đăng nhập'>
                     {getFieldDecorator('username', _fieldDecorator.username)(
                         <Input
-                            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                            placeholder="Username"
-                        />,
+                            prefix={<Icon type='user' style={{color: 'rgba(0,0,0,.25)'}} />}
+                            placeholder='Username'
+                        />
                     )}
                 </Form.Item>
-                <Form.Item label="Mật khẩu">
+                <Form.Item label='Mật khẩu'>
                     {getFieldDecorator('password', _fieldDecorator.password)(
                         <Input
-                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                            type="password"
-                            placeholder="Password"
-                        />,
+                            prefix={<Icon type='lock' style={{color: 'rgba(0,0,0,.25)'}} />}
+                            type='password'
+                            placeholder='Password'
+                        />
                     )}
                 </Form.Item>
-                <Form.Item label="Tuổi">
+                <Form.Item label='Tuổi'>
                     {getFieldDecorator('age', _fieldDecorator.age)(
                         <Select
-                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                            type="password"
-                            placeholder=" Tuổi "
-                        >
-                            <Option value="<20">{'<20'}</Option>
-                            <Option value="20-25">{'20-25'}</Option>
-                            <Option value="25-30">{'25-30'}</Option>
-                            <Option value=">30">{'>30'}</Option>
-
-                        </Select>,
+                            prefix={<Icon type='lock' style={{color: 'rgba(0,0,0,.25)'}} />}
+                            type='password'
+                            placeholder=' Tuổi '>
+                            <Option value='<20'>{'<20'}</Option>
+                            <Option value='20-25'>20-25</Option>
+                            <Option value='25-30'>25-30</Option>
+                            <Option value='>30'>{'>30'}</Option>
+                        </Select>
                     )}
                 </Form.Item>
-                <Form.Item label="Chiều cao">
-                    {getFieldDecorator('height', _fieldDecorator.height)(
-                        <Input
-                            placeholder="Chiều cao"
-                        >
-                        </Input>,
-                    )}
+                <Form.Item label='Chiều cao'>
+                    {getFieldDecorator('height', _fieldDecorator.height)(<Input placeholder='Chiều cao' />)}
                 </Form.Item>
-                <Form.Item label="Địa chỉ">
-                    <Button type='primary' onClick={this.handleGetLocation}>Lấy địa chỉ</Button>
+                <Form.Item label='Địa chỉ'>
+                    <Button type='primary' onClick={this.handleGetLocation}>
+                        Lấy địa chỉ
+                    </Button>
                 </Form.Item>
-                <Form.Item label="Giới tính">
+                <Form.Item label='Giới tính'>
                     {getFieldDecorator('gender', _fieldDecorator.height)(
                         <Select
-                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                            placeholder="Giới tính"
-                        >
-                            <Option value="male">{'Nam'}</Option>
-                            <Option value="female">{'Nữ'}</Option>
-                        </Select>,
+                            prefix={<Icon type='lock' style={{color: 'rgba(0,0,0,.25)'}} />}
+                            placeholder='Giới tính'>
+                            <Option value='male'>Nam</Option>
+                            <Option value='female'>Nữ</Option>
+                        </Select>
                     )}
                 </Form.Item>
-                <Form.Item label="Nghề nghiệp">
+                <Form.Item label='Nghề nghiệp'>
                     {getFieldDecorator('job', _fieldDecorator.height)(
                         <Select
-                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                            placeholder="Nghề nghiệp"
-                        >
-                            <Option value="student">{'Học sinh'}</Option>
-                            <Option value="teacher">{'Giáo viên'}</Option>
-                            <Option value="engineer">{'Kỹ sư'}</Option>
-                            <Option value="doctor">{'Bác sĩ'}</Option>
-                            <Option value="accounting">{'Kế toán'}</Option>
-                            <Option value="developer">{'Lập trình viên'}</Option>
-                            <Option value="seller">{'Nhân viên bán hàng'}</Option>
-                            <Option value="other">{'Khác'}</Option>
-                        </Select>,
+                            prefix={<Icon type='lock' style={{color: 'rgba(0,0,0,.25)'}} />}
+                            placeholder='Nghề nghiệp'>
+                            <Option value='student'>Học sinh</Option>
+                            <Option value='teacher'>Giáo viên</Option>
+                            <Option value='engineer'>Kỹ sư</Option>
+                            <Option value='doctor'>Bác sĩ</Option>
+                            <Option value='accounting'>Kế toán</Option>
+                            <Option value='developer'>Lập trình viên</Option>
+                            <Option value='seller'>Nhân viên bán hàng</Option>
+                            <Option value='other'>Khác</Option>
+                        </Select>
                     )}
                 </Form.Item>
-                <Form.Item label="Môn thể thao">
+                <Form.Item label='Môn thể thao'>
                     {getFieldDecorator('sport', _fieldDecorator.height)(
                         <Select
-                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                            placeholder="Môn thể thao"
-                        >
-                            <Option value="football">{'Bóng đá'}</Option>
-                            <Option value=">basketball">{'Bóng rổ'}</Option>
-                            <Option value=">volleyball">{'Bóng chuyền'}</Option>
-                            <Option value=">swimming">{'Bơi'}</Option>
-                            <Option value=">badminton">{'Cầu lông'}</Option>
-                            <Option value=">table_tenis">{'Bóng bàn'}</Option>
-                            <Option value=">tenis">{'Tennis'}</Option>
-
-                        </Select>,
+                            prefix={<Icon type='lock' style={{color: 'rgba(0,0,0,.25)'}} />}
+                            placeholder='Môn thể thao'>
+                            <Option value='football'>Bóng đá</Option>
+                            <Option value='>basketball'>Bóng rổ</Option>
+                            <Option value='>volleyball'>Bóng chuyền</Option>
+                            <Option value='>swimming'>Bơi</Option>
+                            <Option value='>badminton'>Cầu lông</Option>
+                            <Option value='>table_tenis'>Bóng bàn</Option>
+                            <Option value='>tenis'>Tennis</Option>
+                        </Select>
                     )}
                 </Form.Item>
 
-                <Form.Item>
-                    <Row>
-                        <Col >
-                            <Button type="primary" htmlType="submit" style={{ width: '100%' }} loading={this.state.signUpLoading}>
+                <Row>
+                    <Col>
+                        <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+                            <Button
+                                type='primary'
+                                htmlType='submit'
+                                style={{width: '68%'}}
+                                loading={this.state.signUpLoading}>
                                 Đăng ký
                             </Button>
-
-                        </Col>
-                    </Row>
-
-                </Form.Item>
+                        </div>
+                    </Col>
+                </Row>
             </Form>
         )
     }
 }
-const WrapperSignUpForm = Form.create({ name: 'sign_up' })(SignUpForm);
-
+const WrapperSignUpForm = Form.create({name: 'sign_up'})(SignUpForm)
 
 export default WrapperSignUpForm
---
+++ react-front-end/src/firebase/firebaseUtils.js
@@ -0,0 +1,41 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const config = {
+    apiKey: 'AIzaSyDtciQXj-Cw9CftMd6jcE0NXrtlNVOtk-8',
+    authDomain: 'react30-9ebe0.firebaseapp.com',
+    databaseURL: 'https://react30-9ebe0.firebaseio.com',
+    projectId: 'react30-9ebe0',
+    storageBucket: '',
+    messagingSenderId: '146597253385',
+    appId: '1:146597253385:web:645ece10fd246b02',
+}
+firebase.initializeApp(config)
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+    const snapShot = await userRef.get()
+
+    if (!snapShot.exists) {
+        const {displayName, email} = userAuth
+
+        const createdAt = new Date()
+
+        try {
+            await userRef.set({displayName, email, createdAt, ...additionalData})
+        } catch (error) {
+            console.log('error creating user', error.message)
+        }
+    }
+
+    return userRef
+}
+
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
+
+export default firebase
