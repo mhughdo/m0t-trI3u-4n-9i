fcebdf255a17478031611d650c4ecdb40115d0f8
damdauvaotran <ngokhoaisan1999@gmail.com>
2019-07-28 08:17:28 +0700

    add chat

--- react-front-end/src/pages/chat.js
+++ react-front-end/src/pages/chat.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
+import { Col, Row, Tabs, Avatar } from 'antd'
+import UserChatAvatar from '../components/user-chat/UserChatAvatar'
+import MessageBox from '../components/user-chat/MessageBox';
 
+
+
+const { TabPane } = Tabs;
 class Chat extends Component {
     render() {
         return (
-            <h1>Chat page</h1>
+            <Tabs defaultActiveKey="1" tabPosition='left' style={{ minHeight: '80vh', maxHeight: '90vh' }}>
+                {[...Array(30).keys()].map(i => (
+                    <TabPane tab={<UserChatAvatar />} key={i}>
+                        <MessageBox></MessageBox>
+                    </TabPane>
+                ))}
+            </Tabs>
+
         )
     }
 }
